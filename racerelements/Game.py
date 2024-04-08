bd09d2d59b69a38f36178cf7f4dd4f944c75031e
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-04-09 00:32:46 +0600

    Update Game.py

--- racerelements/Game.py
+++ racerelements/Game.py
@@ -1,52 +1,34 @@
-#Imports
-import pygame, sys
-from pygame.locals import *
-import random, time
+import pygame
+import random
+import time
+import sys
 
-#Initialzing 
 pygame.init()
 
-#Setting up FPS 
-FPS = 60
-FramePerSec = pygame.time.Clock()
-
-#Creating colors
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-#Other Variables for use in the program
+fps = 60
+time1 = pygame.time.Clock()
+font = pygame.font.SysFont("Verdana",60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, (0, 0, 0))
+bg = pygame.image.load("racerelements/background.png")
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
-SPEED = 5
+SPEED = 1
 SCORE = 0
-
-#Setting up Fonts
-font = pygame.font.SysFont("Verdana", 60)
-font_small = pygame.font.SysFont("Verdana", 20)
-game_over = font.render("Game Over", True, BLACK)
-
-background = pygame.image.load("racerelements/background.png")
-
-#Create a white screen 
-DISPLAYSURF = pygame.display.set_mode((400,600))
-DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("Game")
-
-
+SCORE2 = 0 
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Race")
 class Enemy(pygame.sprite.Sprite):
-      def __init__(self):
-        super().__init__() 
+    def __init__(self):
+        super().__init__()
         self.image = pygame.image.load("racerelements/Enemy.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
-      def move(self):
+    def move(self):
         global SCORE
-        self.rect.move_ip(0,SPEED)
-        if (self.rect.bottom > 600):
+        self.rect.move_ip(0,SPEED*5)
+        if self.rect.top > SCREEN_HEIGHT:
             SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
@@ -54,73 +36,79 @@ class Enemy(pygame.sprite.Sprite):
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
         self.image = pygame.image.load("racerelements/Player.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (160, 520)
-       
+        self.rect.center = (160, SCREEN_HEIGHT - 80)
+
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-        
         if self.rect.left > 0:
-              if pressed_keys[K_LEFT]:
-                  self.rect.move_ip(-5, 0)
-        if self.rect.right < SCREEN_WIDTH:        
-              if pressed_keys[K_RIGHT]:
-                  self.rect.move_ip(5, 0)
-                  
+            if pressed_keys[pygame.K_LEFT]:
+                self.rect.move_ip(-10, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[pygame.K_RIGHT]:
+                self.rect.move_ip(10, 0)
 
-#Setting up Sprites        
+
+class Coins(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("racerelements/crash.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        global SCORE2
+        self.rect.move_ip(0, 5)
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
 P1 = Player()
 E1 = Enemy()
-
-#Creating Sprites Groups
+C1 = Coins()
 enemies = pygame.sprite.Group()
 enemies.add(E1)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
-
-#Adding a new User event 
+all_sprites.add(C1)
 INC_SPEED = pygame.USEREVENT + 1
-pygame.time.set_timer(INC_SPEED, 1000)
-
-#Game Loop
+pygame.time.set_timer(INC_SPEED, 5000) 
 while True:
-      
-    #Cycles through all events occuring  
     for event in pygame.event.get():
         if event.type == INC_SPEED:
-              SPEED += 0.5      
-        if event.type == QUIT:
+            SPEED += 0.5
+            new_coin = Coins()  
+            all_sprites.add(new_coin)
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-
-    DISPLAYSURF.blit(background, (0,0))
-    scores = font_small.render(str(SCORE), True, BLACK)
-    DISPLAYSURF.blit(scores, (10,10))
-
-    #Moves and Re-draws all Sprites
+    DISPLAYSURF.blit(bg, (0, 0))
+    scores = font_small.render("Score: " + str(SCORE), True, (0, 0, 0))
+    coin_score = font_small.render("Coins: " + str(SCORE2), True, (0, 0, 0))
+    DISPLAYSURF.blit(scores, (10, 10))
+    DISPLAYSURF.blit(coin_score, (SCREEN_WIDTH - 200, 10))
     for entity in all_sprites:
-        entity.move()
-        DISPLAYSURF.blit(entity.image, entity.rect)
-        
-
-    #To be run if collision occurs between Player and Enemy
+        if entity != P1:
+            DISPLAYSURF.blit(entity.image, entity.rect)
+            entity.move()
+    DISPLAYSURF.blit(P1.image, P1.rect)
+    P1.move()
     if pygame.sprite.spritecollideany(P1, enemies):
-          pygame.mixer.Sound('racerelements/crash.wav').play()
-          time.sleep(1)
-                   
-          DISPLAYSURF.fill(RED)
-          DISPLAYSURF.blit(game_over, (30,250))
-          
-          pygame.display.update()
-          for entity in all_sprites:
-                entity.kill() 
-          time.sleep(2)
-          pygame.quit()
-          sys.exit()        
-        
+        pygame.mixer.Sound("racerelements/crash.wav").play()
+        time.sleep(0.5)
+        DISPLAYSURF.blit(game_over, (30,250))
+        pygame.display.flip()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(1)
+        pygame.quit()
+        sys.exit()
+    collisions = pygame.sprite.spritecollide(P1, all_sprites, True)
+    print(SCORE2)
+    for coin in collisions: 
+        SCORE2 += random.randint(1,3)
     pygame.display.update()
-    FramePerSec.tick(FPS)
+    time1.tick(fps)
